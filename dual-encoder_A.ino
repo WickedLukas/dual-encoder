8c7abe7ab56e6be4ff94e54c48665ef44368b8d0
Lukas <lukas_m@gmx.de>
2017-12-23 10:32:47 +0100

    Added dual-encoder_A.ino and dual-encoder_AB.ino

---
+++ dual-encoder_A.ino
@@ -0,0 +1,91 @@
+/*
+* dual-encoder_A.ino
+*
+* Created: 23.12.2017
+* Author: Lukas
+*
+* Based on "Efficiently Reading Quadrature With Interrupts"
+* http://makeatronics.blogspot.de/2013/02/efficiently-reading-quadrature-with.html
+*
+*/
+
+#include "Arduino.h"
+#include "Wire.h"
+
+#define SLAVE_ADDRESS 0x08
+
+// volatile boolean M1_interrupt = false;
+// volatile boolean M2_interrupt = false;
+
+volatile int8_t enc_count_M1 = 0;
+volatile int8_t enc_count_M2 = 0;
+
+// stores which combination of current and previous encoder state lead to an increase or decrease of the encoder count and which are not defined
+const int8_t lookup_table[] = {0, 0, 0, 0, 0, -1, 1, 0, 0, 1, -1, 0, 0, 0, 0, 0};
+	
+void requestEvent() {
+	Wire.write(enc_count_M1);
+	Wire.write(enc_count_M2);
+	
+	enc_count_M1 = 0;
+	enc_count_M2 = 0;
+}
+
+void encoder_isr_M1() {
+	// stores the current and previous state of both encoder channels (A, B)
+	static uint8_t enc_state = 0;
+	
+	// shift the previous encoder state to the left
+	enc_state = enc_state << 1;
+	// add the current encoder state (PIND stores the values of digital pins 0-7)
+	enc_state = (enc_state & 0b1010) | ((PIND & 0b10100) >> 2);
+	
+	// change encounter count according to encoder state
+	enc_count_M1 = enc_count_M1 + lookup_table[enc_state & 0b1111];
+	
+	//M1_interrupt = true;
+}
+
+void encoder_isr_M2() {
+	// stores the current and previous state of both encoder channels (A, B)
+	static uint8_t enc_state = 0;
+	
+	// shift the previous encoder state to the left
+	enc_state = enc_state << 1;
+	// add the current encoder state (PIND stores the values of digital pins 0-7)
+	enc_state = (enc_state & 0b1010) | ((PIND & 0b101000) >> 3);
+	
+	// change encounter count according to encoder state
+	enc_count_M2 = enc_count_M2 - lookup_table[enc_state & 0b1111];
+	
+	//M2_interrupt = true;
+}
+
+void setup() {
+	// join i2c bus
+	Wire.begin(SLAVE_ADDRESS);
+	// register event
+	Wire.onRequest(requestEvent);
+	
+	// initialize serial communication
+	Serial.begin(115200);
+	while (!Serial); // wait for Leonardo eNUMeration, others continue immediately
+
+	attachInterrupt(digitalPinToInterrupt(2), encoder_isr_M1, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(3), encoder_isr_M2, CHANGE);´
+
+	digitalWrite(digitalPinToInterrupt(2), HIGH);
+	digitalWrite(digitalPinToInterrupt(3), HIGH);
+}
+
+void loop() {
+	/*if (M1_interrupt) {
+		Serial.println(enc_count_M1 % 1000);
+		M1_interrupt = false;
+	}
+	
+	if (M2_interrupt) {
+		Serial.print("\t"); Serial.println(enc_count_M2 % 1000);
+		M2_interrupt = false;
+	}*/
+}
