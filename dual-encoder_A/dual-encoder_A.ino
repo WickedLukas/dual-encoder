b0c2b4508553dea2ab8f2487b0d97e42c10273cb
Lukas <lukas_m@gmx.de>
2019-01-01 11:24:35 +0100

    Cleanup.

--- dual-encoder_A/dual-encoder_A.ino
+++ dual-encoder_A/dual-encoder_A.ino
@@ -14,13 +14,11 @@
 
 #define SLAVE_ADDRESS 0x08
 
-//volatile boolean M1_interrupt = false;
-//volatile boolean M2_interrupt = false;
+//volatile boolean enc_M1_interrupt = false;
+//volatile boolean enc_M2_interrupt = false;
 
-volatile int16_t enc_count_M1 = 0;
-volatile int16_t enc_count_M2 = 0;
-
-byte message[4];
+volatile int16_t enc_count_M1 = 0;	// motor 1 encoder count
+volatile int16_t enc_count_M2 = 0;	// motor 2 encoder count
 
 // stores which combination of current and previous encoder state lead to an increase or decrease of the encoder count and which are not defined
 const int8_t lookup_table[] = {0, 0, 0, 0, 0, -1, 1, 0, 0, 1, -1, 0, 0, 0, 0, 0};
@@ -40,27 +38,21 @@ const int8_t lookup_table[] = {0, 0, 0, 0, 0, -1, 1, 0, 0, 1, -1, 0, 0, 0, 0, 0}
 #endif
 
 void requestEvent() {
-	static uint8_t SREG_bak;
+	static byte message[6];
 	
-	SREG_bak = SREG;	//save global interrupt state
-	noInterrupts();
-	message[0] = (enc_count_M1 >> 8) & 0xFF;
-	message[1] = enc_count_M1 & 0xFF;
-	SREG = SREG_bak; 	//restore interrupt state
-	
-	SREG_bak = SREG;	//save global interrupt state
-	noInterrupts();
-	message[2] = (enc_count_M2 >> 8) & 0xFF;
-	message[3] = enc_count_M2 & 0xFF;
-	SREG = SREG_bak; 	//restore interrupt state
+	message[0] = (enc_count_M1 >> 8) & 0xFF;	// most significant bits for motor 1 encoder count
+	message[1] = enc_count_M1 & 0xFF;			// least significant bits for motor 1 encoder count
+	message[2] = (enc_count_M2 >> 8) & 0xFF;	// most significant bits for motor 2 encoder count
+	message[3] = enc_count_M2 & 0xFF;			// least significant bits for motor 2 encoder count
 
+	Wire.write(message, 4);
+
+	// reset encoder count
 	enc_count_M1 = 0;
 	enc_count_M2 = 0;
-	
-	Wire.write(message, 4);
 }
 
-void encoder_isr_M1() {
+void enc_isr_M1() {
 	// stores the current and previous state of both encoder channels (A, B)
 	static uint8_t enc_state = 0;
 	
@@ -69,13 +61,13 @@ void encoder_isr_M1() {
 	// add the current encoder state (PIND stores the values of digital pins 0-7)
 	enc_state = (enc_state & 0b1010) | ((PIND & 0b10100) >> 2);
 	
-	// change encounter count according to encoder state
+	// update encoder count according to the encoder state
 	enc_count_M1 = enc_count_M1 + lookup_table[enc_state & 0b1111];
 	
-	//M1_interrupt = true;
+	//enc_M1_interrupt = true;
 }
 
-void encoder_isr_M2() {
+void enc_isr_M2() {
 	// stores the current and previous state of both encoder channels (A, B)
 	static uint8_t enc_state = 0;
 	
@@ -84,10 +76,10 @@ void encoder_isr_M2() {
 	// add the current encoder state (PIND stores the values of digital pins 0-7)
 	enc_state = (enc_state & 0b1010) | ((PIND & 0b101000) >> 3);
 	
-	// change encounter count according to encoder state
+	// update encoder count according to the encoder state
 	enc_count_M2 = enc_count_M2 - lookup_table[enc_state & 0b1111];
 	
-	//M2_interrupt = true;
+	//enc_M2_interrupt = true;
 }
 
 void setup() {
@@ -102,21 +94,22 @@ void setup() {
 	while (!Serial); // wait for Leonardo eNUMeration, others continue immediately
 	#endif
 	
-	pinMode(2, INPUT);
-	pinMode(3, INPUT);
+	// prepare encoder pins
+	pinMode(2, INPUT);	// interrupt pin for encoder M1
+	pinMode(3, INPUT);	// interrupt pin for encoder M2
 	
-	attachInterrupt(digitalPinToInterrupt(2), encoder_isr_M1, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(3), encoder_isr_M2, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(2), enc_isr_M1, CHANGE);	// attach interrupt service routine enc_isr_M1 to encoder M1 interrupt pin
+	attachInterrupt(digitalPinToInterrupt(3), enc_isr_M2, CHANGE);	// attach interrupt service routine enc_isr_M2 to encoder M2 interrupt pin
 }
 
 void loop() {
-	/*if (M1_interrupt) {
+	/*if (enc_M1_interrupt) {
 		DEBUG_PRINTLN(enc_count_M1 % 1000);
-		M1_interrupt = false;
+		enc_M1_interrupt = false;
 	}
 	
-	if (M2_interrupt) {
+	if (enc_M2_interrupt) {
 		DEBUG_PRINT("\t"); DEBUG_PRINTLN(enc_count_M2 % 1000);
-		M2_interrupt = false;
+		enc_M2_interrupt = false;
 	}*/
 }
